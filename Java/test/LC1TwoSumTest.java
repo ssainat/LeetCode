f1baf0aafef76fd6afb3a66a150321b4036f025a
Shraddha Sainath <>
2023-07-17 15:37:17 -0400

    two sum

---
+++ Java/test/LC1TwoSumTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LC1TwoSumTest {
+
+    @Test
+    void twoSum() {
+        LC1TwoSum ts = new LC1TwoSum();
+        int[] nums = new int[]{2,7,11,15};
+        int target = 9;
+        int[] finalResult = new int[]{0,1};
+        int[] result = ts.twoSum(nums, target);
+        assertArrayEquals(finalResult, result);
+    }
+}
